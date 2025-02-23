4ca8ae805b3ea52de11086a8c9579326097b7ef2
wz2cool <11556152+wz2cool@users.noreply.github.com>
2025-02-23 10:37:31 +0800

    Update superset_config.py

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -2,3 +2,10 @@ ENABLE_PROXY_FIX = True
 SECRET_KEY = "MyVerySecretKey"
 PREVENT_UNSAFE_DB_CONNECTIONS = False
 TALISMAN_ENABLED = False
+# 设置默认语言为中文
+BABEL_DEFAULT_LOCALE = "zh"
+# 启用语言切换
+LANGUAGES = {
+    "en": {"flag": "us", "name": "English"},
+    "zh": {"flag": "cn", "name": "中文"},
+}
