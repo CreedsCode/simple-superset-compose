cae0363f9667245d599c224ed176e1ab43d8b396
wz2cool <11556152+wz2cool@users.noreply.github.com>
2025-02-23 10:38:26 +0800

    Update superset_config.py

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -2,9 +2,10 @@ ENABLE_PROXY_FIX = True
 SECRET_KEY = "MyVerySecretKey"
 PREVENT_UNSAFE_DB_CONNECTIONS = False
 TALISMAN_ENABLED = False
-# 设置默认语言为中文
+PUBLIC_ROLE_LIKE = "Gamma"
+HTTP_HEADERS = {}
+ WTF_CSRF_ENABLED = False
 BABEL_DEFAULT_LOCALE = "zh"
-# 启用语言切换
 LANGUAGES = {
     "en": {"flag": "us", "name": "English"},
     "zh": {"flag": "cn", "name": "中文"},
